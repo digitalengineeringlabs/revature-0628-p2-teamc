f166453016c1ac31dc5dea1dafeb2ad048834979
SeraphimThomas <foxboyseraphim@gmail.com>
2021-08-20 08:19:07 -0500

    First commit with basic setup
    Just the variables and some setup in interface

---
+++ Project_2/src/main/java/main/Project_2.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project_2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project_2.class, args);
+	}
+
+}
