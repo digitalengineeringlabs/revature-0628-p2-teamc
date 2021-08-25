3ecfb9f35d23cf9cf0ce65b4ac1b51f2f725cc17
zyon-revature <zyon.savery@revature.net>
2021-08-25 12:16:40 -0500

    Empty login page

--- Project2App/src/app/app.module.ts
+++ Project2App/src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from "./app.component";
+import {Routes, RouterModule} from "@angular/router";
+import {LoginComponent} from "./login/login.component";
 
-import { AppComponent } from './app.component';
+const routes: Routes = [
+  {path: '', component: LoginComponent}
+]
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
+  declarations: [AppComponent],
+  imports: [RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
 })
-export class AppModule { }
+
+// @ts-ignore
+export class AppModule {
+}
