da78227932148a0cab739483cf50ca7a23b5fcdf
zyon-revature <zyon.savery@revature.net>
2021-08-26 11:30:18 -0500

    Plain login webpage

--- Project2App/src/app/app.module.ts
+++ Project2App/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from "./app.component";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 import {Routes, RouterModule} from "@angular/router";
 import {LoginComponent} from "./login/login.component";
 
@@ -10,9 +12,15 @@ const routes: Routes = [
 
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [RouterModule.forRoot(routes),
-    BrowserModule
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
@@ -22,4 +30,3 @@ const routes: Routes = [
 // @ts-ignore
 export class AppModule {
 }
-
--- Project2App/src/app/login/login.component.ts
+++ Project2App/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {NgForm} from "@angular/forms";
+import {Route, Router} from "@angular/router";
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginUrl = "http://localhost:8080/createuser/login"
+  invalid: boolean = false;
+
+  constructor(private http: HttpClient, private router: Router) {
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(JSON.stringify({username: form.value.username, userpassword: form.value.password}))
+    this.http.post(this.loginUrl, {username: form.value.username, userpassword: form.value.password})
+      .subscribe({
+        next: (data: any) => {
+          if (data != null) {
+            localStorage.setItem("id", data.userid);
+            localStorage.setItem("manager", data.manager);
+            //this.router.navigate(['home']);
+            console.log("Login Worked");
+          } else {
+            this.invalid = true;
+            console.log("Login Did Not Work");
+          }
+        }
+
+      })
   }
 
 }
