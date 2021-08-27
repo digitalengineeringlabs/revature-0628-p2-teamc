3f22e7d4791301d40035376826af91a1ffd5f1df
e-Henderson <eddie.henderson@revature.net>
2021-08-26 23:31:13 -0400

    5th commit to ticket association angular

--- Project2App/src/app/app-routing.module.ts
+++ Project2App/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TicketassociationComponent } from './ticketassociation/ticketassociation.component';
 const routes: Routes=[
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   {path: 'tickets', component: TicketassociationComponent},
 ];
 
